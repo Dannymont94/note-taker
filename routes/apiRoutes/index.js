a8901bf3dddde2cc736c847a1495f681cff98b72
Daniel Monterrosa <65088117+Dannymont94@users.noreply.github.com>
2020-08-12 22:55:06 -0400

    api/notes post route adds validated note to db.json file and then returns the new note to the client

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const uuid = require('uuid');
 const { notes } = require('../../data/db.json');
-const { validateNote } = require('../../lib/notes');
+const { validateNote, createNewNote } = require('../../lib/notes');
 
 // create GET /api/notes route to read db.json and return all saved notes as JSON
 router.get('/notes', (req, res) => {
@@ -10,14 +10,16 @@ router.get('/notes', (req, res) => {
 
 // create POST /api/notes route to recieve a new note to save on the request body, add it to the db.json file, and then return the new note to the client.
 router.post('/notes', (req, res) => {
+  // need to give each note a unique id when it's saved
   req.body.id = uuid.v4();
   
   if (!validateNote(req.body)) {
     res.status(400).send('Please make sure your note has a title and body text before saving.');
   } else {
-    
+    const note = createNewNote(req.body, notes);
+    res.json(note);
+  }
 });  
-// need to give each note a unique id when it's saved
 
 // BONUS: create DELETE api/notes/:id route containing the id of a note to delete. Will need to read all notes from the db.json file, 
   // remove the note with the given id property, and then rewrite the notes to the db.json file.
