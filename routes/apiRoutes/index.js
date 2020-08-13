7ab59efed36d14b5cb69168fa430d43c0f7730c4
Daniel Monterrosa <65088117+Dannymont94@users.noreply.github.com>
2020-08-12 21:56:27 -0400

    html routes working

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,7 +1,11 @@
+const router = require('express').Router();
+const { notes } = require('../../data/db.json');
 // create GET /api/notes route to read db.json and return all saved notes as JSON
 
 // create POST /api/notes route to recieve a new note to save on the request body, add it to the db.json file, and then return the new note to the client.
   // need to give each note a unique id when it's saved
 
-// BONUS: create DELET api/notes/:id route containing the id of a note to delete. Will need to read all notes from the db.json file, 
+// BONUS: create DELETE api/notes/:id route containing the id of a note to delete. Will need to read all notes from the db.json file, 
   // remove the note with the given id property, and then rewrite the notes to the db.json file.
+
+module.exports = router;
--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -1,3 +1,14 @@
+const path = require('path');
+const router = require('express').Router();
+
 // GET /notes should return the notes.html file
+router.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../public/notes.html'));
+});
 
 // Get * should return the index.html file
+router.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../public/index.html'));
+});
+
+module.exports = router;
