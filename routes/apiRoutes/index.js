3a9a821cfb29c917b81868aab1e8fb43ed9a8b88
Daniel Monterrosa <65088117+Dannymont94@users.noreply.github.com>
2020-08-12 21:40:16 -0400

    added server boilerplate and some routing pseudocode

---
+++ routes/apiRoutes/index.js
@@ -0,0 +1,7 @@
+// create GET /api/notes route to read db.json and return all saved notes as JSON
+
+// create POST /api/notes route to recieve a new note to save on the request body, add it to the db.json file, and then return the new note to the client.
+  // need to give each note a unique id when it's saved
+
+// BONUS: create DELET api/notes/:id route containing the id of a note to delete. Will need to read all notes from the db.json file, 
+  // remove the note with the given id property, and then rewrite the notes to the db.json file.
---
+++ routes/htmlRoutes/index.js
@@ -0,0 +1,3 @@
+// GET /notes should return the notes.html file
+
+// Get * should return the index.html file
