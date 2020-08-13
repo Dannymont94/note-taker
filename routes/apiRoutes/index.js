91d1b6b75b47da1638630847077dfaf9b77419a0
Daniel Monterrosa <65088117+Dannymont94@users.noreply.github.com>
2020-08-12 22:36:41 -0400

    successfully generating random id for each req.body that comes through a post request

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { db } = require('../../data/db.json');
+const uuid = require('uuid');
 
 // create GET /api/notes route to read db.json and return all saved notes as JSON
 router.get('/notes', (req, res) => {
@@ -7,7 +8,11 @@ router.get('/notes', (req, res) => {
 });
 
 // create POST /api/notes route to recieve a new note to save on the request body, add it to the db.json file, and then return the new note to the client.
-  // need to give each note a unique id when it's saved
+router.post('/notes', (req, res) => {
+  req.body.id = uuid.v4();
+  res.send(req.body);
+});  
+// need to give each note a unique id when it's saved
 
 // BONUS: create DELETE api/notes/:id route containing the id of a note to delete. Will need to read all notes from the db.json file, 
   // remove the note with the given id property, and then rewrite the notes to the db.json file.
