699898c8b5a3de49612b44f44babffd7a15cd621
Daniel Monterrosa <65088117+Dannymont94@users.noreply.github.com>
2020-08-12 22:14:01 -0400

    C:/Program Files/Git/notes apiRoutes endpoint is properly retrieving db.json data

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,6 +1,10 @@
 const router = require('express').Router();
-const { notes } = require('../../data/db.json');
+const { db } = require('../../data/db.json');
+
 // create GET /api/notes route to read db.json and return all saved notes as JSON
+router.get('/notes', (req, res) => {
+  res.json(db);
+});
 
 // create POST /api/notes route to recieve a new note to save on the request body, add it to the db.json file, and then return the new note to the client.
   // need to give each note a unique id when it's saved
